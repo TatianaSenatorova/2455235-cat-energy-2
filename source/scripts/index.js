d8d0daafb8ba3a96669aa09cbb36ffcdf2ee4911
TatianaSenatorova <108460915+TatianaSenatorova@users.noreply.github.com>
2024-07-21 02:50:45 +0400

    Добавляет бургер-меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
 /* в этот файл добавляет скрипты*/
+
+const burgerEl = document.querySelector('.burger');
+const navListEl = document.querySelector('.nav-list');
+
+burgerEl.addEventListener('click', () => {
+  burgerEl.classList.toggle('is-opened');
+  navListEl.classList.toggle('is-opened');
+});
