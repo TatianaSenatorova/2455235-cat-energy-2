7557824d3272efedd6d002abcb034a04cd4ee3e1
TatianaSenatorova <108460915+TatianaSenatorova@users.noreply.github.com>
2024-07-21 19:21:07 +0400

    Корректирует header

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,6 @@
 /* в этот файл добавляет скрипты*/
 
+//Открытие и закрытие меню
 const burgerEl = document.querySelector('.burger');
 const navListEl = document.querySelector('.nav-list');
 
@@ -7,3 +8,4 @@ burgerEl.addEventListener('click', () => {
   burgerEl.classList.toggle('is-opened');
   navListEl.classList.toggle('is-opened');
 });
+
