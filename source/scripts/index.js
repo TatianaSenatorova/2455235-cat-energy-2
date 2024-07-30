1fbe85030fa711c7b5ef6f4a552e4ed30d26dfc5
TatianaSenatorova <108460915+TatianaSenatorova@users.noreply.github.com>
2024-07-30 14:24:40 +0400

    Закончена стилизация секции experience

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -11,54 +11,54 @@ burgerEl.addEventListener('click', () => {
 
 //Слайдер
 
-const sliderDividerEl = document.querySelector('.slider__divider');
-const sliderZoneEl = document.querySelector('.slider');
-// const sliderAfterEl = document.querySelector('.slider__item-after');
-const sliderBeforeEl = document.querySelector('.slider__item-after');
+// const sliderDividerEl = document.querySelector('.slider__divider');
+// const sliderZoneEl = document.querySelector('.slider');
+// // const sliderAfterEl = document.querySelector('.slider__item-after');
+// const sliderBeforeEl = document.querySelector('.slider__item-after');
 
-let mouseEvent = false;
+// let mouseEvent = false;
 
-// Начало события изменения размера половинки слайда
-sliderDividerEl.addEventListener('mousedown', (e) => {
-  e.preventDefault();
-  mouseEvent = true;
-  console.log(mouseEvent);
-});
+// // Начало события изменения размера половинки слайда
+// sliderDividerEl.addEventListener('mousedown', (e) => {
+//   e.preventDefault();
+//   mouseEvent = true;
+//   console.log(mouseEvent);
+// });
 
-// Завершение события изменения размера половинки слайда
-sliderDividerEl.addEventListener('mouseup', (e) => {
-  e.preventDefault();
-  mouseEvent = false;
-});
+// // Завершение события изменения размера половинки слайда
+// sliderDividerEl.addEventListener('mouseup', (e) => {
+//   e.preventDefault();
+//   mouseEvent = false;
+// });
 
-// Завершение события изменения размера половинки слайда
-sliderZoneEl.addEventListener('mouseup', () => {
-  mouseEvent = false;
-});
+// // Завершение события изменения размера половинки слайда
+// sliderZoneEl.addEventListener('mouseup', () => {
+//   mouseEvent = false;
+// });
 
-// Завершение события изменения размера половинки слайда
-sliderZoneEl.addEventListener('mouseleave', () => {
-  mouseEvent = false;
-});
+// // Завершение события изменения размера половинки слайда
+// sliderZoneEl.addEventListener('mouseleave', () => {
+//   mouseEvent = false;
+// });
 
-// Отслеживаем движения мыши в области слайдера
-sliderZoneEl.addEventListener('mousemove', (e) => {
-  console.log(sliderZoneEl.offsetLeft, e.pageX, sliderZoneEl.offsetWidth);
-  const x = e.pageX - sliderZoneEl.offsetLeft;
-  let percent = x * 100 / sliderZoneEl.offsetWidth;
-  console.log(percent);
+// // Отслеживаем движения мыши в области слайдера
+// sliderZoneEl.addEventListener('mousemove', (e) => {
+//   console.log(sliderZoneEl.offsetLeft, e.pageX, sliderZoneEl.offsetWidth);
+//   const x = e.pageX - sliderZoneEl.offsetLeft;
+//   let percent = x * 100 / sliderZoneEl.offsetWidth;
+//   console.log(percent);
 
-  // Ограничения
-  if (percent < 0) {
-    percent = 0;
-  } else if (percent > 100) {
-    percent = 100;
-  }
+//   // Ограничения
+//   if (percent < 0) {
+//     percent = 0;
+//   } else if (percent > 100) {
+//     percent = 100;
+//   }
 
-  if (mouseEvent) {
-    console.log(percent);
-    sliderDividerEl.style.left = `${percent }%`;
-    sliderBeforeEl.style.width = `${percent }%`;
-  }
-});
+//   if (mouseEvent) {
+//     console.log(percent);
+//     sliderDividerEl.style.left = `${percent }%`;
+//     sliderBeforeEl.style.width = `${percent }%`;
+//   }
+// });
 
